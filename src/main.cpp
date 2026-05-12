61ee9f4668ccb20e2dbc669d068368de8c1030a3
Jalen Thornhill <by.jalen.by@outlook.com>
2026-05-12 09:55:14 -0400

    feat: added main menu

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+/**
+ * File: main.cpp
+ * Author: Jalen Thornhill
+ * Created: 2026-05-12
+ * Last Modified: 2026-05-12
+ *
+ **/
+
+#include <iostream>
+#include<fstream>
+#include "ReservationSystem.hpp"
+
+
+using namespace std;
+int main{
+    // create ReservationSystem object
+    ReservationSystem reservetionSys
+    // load tables from data/tables.txt
+    fsteam tableFile("../data/tables.txt");
+    // load reservations from data/reservations.csv
+    fsteam reservationFile("../data/reservations.csv");
+
+
+    // menu loop
+int choice;
+do{
+
+cout << "===== Restaurant Seating Management System =====\n";
+cout << "1. Add reservation\n";
+cout << "2. Cancel reservation\n";
+cout << "3. Search by customer name\n";
+cout << "4. Show Time Slot Overview\n";
+cout << "5. Show Maître-d Report\n";
+cout << "6. Show Capacity Utilization Summary\n";
+cout << "7. Export Maître-d Report\n";
+cout << "8. Save & Exit\n";
+cout << "Choose an option: ";
+cin >> choice;
+
+
+switch (choice){
+case 1:
+    break;
+case 2:
+    break;
+case 3:
+    break;
+case 4:
+    break;  
+case 5:
+    break;
+case 6:
+    break;
+case 7:
+    break;
+case 8:
+
+    cout << "Thank you for using Jalen's Restaurant Seating Management System!\n";
+    cout << "saving data and exiting...\n";
+    break;
+
+default:
+    cout << "Invalid choice. :( Please try again.\n";
+    
+}
+} while (choice != 8);
+
+
+
+    return 0;
+}
